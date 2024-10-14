6f921c90d978f7ed59877886591b0e95d55c0695
akbarshah19 <105868459+akbarshah19@users.noreply.github.com>
2024-10-14 08:45:55 +0500

    Initial Commit

---
+++ AdvancedMaps/AdvancedMapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvancedMapsApp.swift
+//  AdvancedMaps
+//
+//  Created by Akbarshah Jumanazarov on 10/14/24.
+//
+
+import SwiftUI
+
+@main
+struct AdvancedMapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
