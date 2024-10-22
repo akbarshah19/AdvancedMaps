927953098b6bba03f555762a0627e49aa55fc4c2
akbarshah19 <105868459+akbarshah19@users.noreply.github.com>
2024-10-22 11:57:54 +0500

    DirectionsVC navBar UI update

--- AdvancedMaps/Directions/DirectionsController.swift
+++ AdvancedMaps/Directions/DirectionsController.swift
@@ -14,9 +14,16 @@ class DirectionsController: UIViewController {
     
     let mapView = MKMapView()
     let navBar = UIView(backgroundColor: .blue)
-    
+    let startTextField = IndentedTextField(placeholder: "Starting Point",
+                                           padding: 12,
+                                           cornerRadius: 8)
+    let endTextField = IndentedTextField(placeholder: "Ending Point",
+                                        padding: 12,
+                                        cornerRadius: 8)
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationController?.navigationBar.isHidden = true
         view.addSubview(mapView)
         mapView.delegate = self
         mapView.showsUserLocation = true
@@ -39,6 +46,39 @@ class DirectionsController: UIViewController {
         navBar.backgroundColor = .blue
         navBar.setupShadow(opacity: 0.5, color: .black)
         navBar.anchor(top: view.topAnchor, leading: view.leadingAnchor, bottom: view.safeAreaLayoutGuide.topAnchor, trailing: view.trailingAnchor, padding: .init(top: 0, left: 0, bottom: -100, right: 0))
+        
+        [startTextField, endTextField].forEach { tf in
+            tf.backgroundColor = .white
+            tf.textColor = .black
+        }
+        
+        let containerView = UIView()
+        navBar.addSubview(containerView)
+        containerView.fillSuperviewSafeAreaLayoutGuide()
+        
+        let startIcon = UIImageView(image: #imageLiteral(resourceName: "start_location_circles").withRenderingMode(.alwaysTemplate),
+                                    contentMode: .scaleAspectFit)
+        startIcon.constrainWidth(20)
+        startIcon.tintColor = .white
+        let endIcon = UIImageView(image: #imageLiteral(resourceName: "annotation_icon").withRenderingMode(.alwaysTemplate),
+                                  contentMode: .scaleAspectFit)
+        endIcon.constrainWidth(20)
+        endIcon.tintColor = .white
+        
+        containerView.stack(containerView.hstack(startIcon, startTextField, spacing: 12),
+                            containerView.hstack(endIcon, endTextField, spacing: 12),
+                            spacing: 12,
+                            distribution: .fillEqually)
+        .withMargins(.init(top: 0, left: 12, bottom: 12, right: 12))
+        
+        startTextField.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleChangeStartLocation)))
+    }
+    
+    @objc
+    fileprivate func handleChangeStartLocation() {
+        let vc = UIViewController()
+        vc.view.backgroundColor = .yellow
+        navigationController?.pushViewController(vc, animated: true)
     }
     
     fileprivate func setupStartEndAnnotation() {
@@ -72,7 +112,7 @@ class DirectionsController: UIViewController {
             response?.routes.forEach({ route in
                 self.mapView.addOverlay(route.polyline)
             })
-            
+             
 //            guard let route = response?.routes.first else { return }
 //            self.mapView.addOverlay(route.polyline)
         }
@@ -107,9 +147,9 @@ extension DirectionsController: MKMapViewDelegate {
 })
 
 struct DirectionsControllerRepresentable: UIViewControllerRepresentable {
-    func makeUIViewController(context: Context) -> DirectionsController {
-        return DirectionsController()
+    func makeUIViewController(context: Context) -> UIViewController {
+        return UINavigationController(rootViewController: DirectionsController())
     }
     
-    func updateUIViewController(_ uiViewController: DirectionsController, context: Context) {}
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
 }
